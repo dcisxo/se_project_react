1570391cdd52b6d14adcc68659e27aaa091087d8
Damian <dcamp090799@gmail.com>
2026-01-07 20:30:50 -0500

    feat: refactor ItemModal to remove like button and adjust delete button logic

--- src/components/ItemModal/ItemModal.jsx
+++ src/components/ItemModal/ItemModal.jsx
@@ -3,23 +3,11 @@ import "./ItemModal.css";
 import closeIcon from "../../assets/close.png";
 import CurrentUserContext from "../../contexts/CurrentUserContext";
 
-function ItemModal({ activeModal, card, onClose, onDeleteClick, onCardLike }) {
+function ItemModal({ activeModal, card, onClose, onDeleteClick }) {
   const currentUser = useContext(CurrentUserContext);
 
   // Checking if the current user is the owner of the current clothing item
   const isOwn = card.owner === currentUser?._id;
-
-  // Check if the item was liked by the current user
-  const isLiked = card.likes?.some((id) => id === currentUser?._id);
-
-  // Create className for like button
-  const modalLikeButtonClassName = `modal__like-button ${
-    isLiked ? "modal__like-button_active" : ""
-  }`;
-
-  const handleLike = () => {
-    onCardLike({ id: card._id, isLiked });
-  };
 
   return (
     <div className={`modal ${activeModal === "preview" && "modal_opened"}`}>
@@ -31,23 +19,16 @@ function ItemModal({ activeModal, card, onClose, onDeleteClick, onCardLike }) {
         <div className="modal__info">
           <div className="modal__header">
             <h2 className="modal__title">{card.name}</h2>
-            {currentUser && (
+            {isOwn && (
               <button
-                className={modalLikeButtonClassName}
-                onClick={handleLike}
-                type="button"
-              />
+                className="modal__delete-btn"
+                onClick={() => onDeleteClick(card)}
+              >
+                Delete item
+              </button>
             )}
           </div>
           <p className="modal__weather">Weather: {card.weather}</p>
-          {isOwn && (
-            <button
-              className="modal__delete-btn"
-              onClick={() => onDeleteClick(card)}
-            >
-              Delete item
-            </button>
-          )}
         </div>
       </div>
     </div>
